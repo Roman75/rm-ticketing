1d516b82e0b4ed4713ef39c272ea39543f241e5c
Roman Marlovits <roman.marlovits@gmail.com>
2019-10-20 14:06:44 +0200

    add file to get states of each repo

---
+++ git-status.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source ./.env
+
+for repo in "${REPOSITORIES[@]}"
+do
+    :
+    cd $repo
+    echo "==========================="
+    echo $repo
+    echo "==========================="
+    git status
+    cd ..
+done
