f49f88e39269bb0a486b131c9684767fdd6adb81
Roman Marlovits <roman.marlovits@gmail.com>
2019-10-29 19:24:56 +0100

    scripts for create repo and all repos

--- release/all.sh
+++ release/all.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
+echo "";
+echo "USAGE:";
+echo "sh all.sh [patch (DEFAULT)|minor|major]";
+echo "";
+echo "EXAMPLES:";
+echo "sh all.sh (bump 0.0.1)";
+echo "sh all.sh patch (bump 0.0.1)";
+echo "sh all.sh minor (bump 0.1.0)";
+echo "sh all.sh major (bump 1.0.0)";
+echo "";
+
+if [ -z "$1" ]
+then
+    TYPE="patch"
+else
+    TYPE=$2
+fi
+
 source ../.env
 
 for repo in "${REPOSITORIES[@]}"
 do
     :
-    sh repo.sh $repo
+    sh repo.sh $repo $TYPE
 done
 
--- release/repo.sh
+++ release/repo.sh
@@ -3,19 +3,26 @@
 if [ -z "$1" ]
 then
     echo "";
-    echo "usage for this file:";
-    echo "sh release.sh REPOSITORY";
+    echo "USAGE:";
+    echo "sh repo.sh REPOSITORY [patch|minor|major]";
     echo "";
-    echo "examples:";
-    echo "sh release.sh rm-ticketing-node-server";
-    echo "sh release.sh rm-ticketing-mysql-server";
-    echo "sh release.sh rm-ticketing-admin";
-    echo "sh release.sh rm-ticketing-promoter";
-    echo "sh release.sh rm-ticketing-scanner";
-    echo "sh release.sh rm-ticketing-page";
-    echo "sh release.sh rm-ticketing-tests";
+    echo "EXAMPLES:";
+    echo "sh repo.sh rm-ticketing-node-server patch";
+    echo "sh repo.sh rm-ticketing-mysql-server patch";
+    echo "sh repo.sh rm-ticketing-admin patch";
+    echo "sh repo.sh rm-ticketing-promoter patch";
+    echo "sh repo.sh rm-ticketing-scanner patch";
+    echo "sh repo.sh rm-ticketing-page patch";
+    echo "sh repo.sh rm-ticketing-tests patch";
     echo "";
     exit 1
+fi
+
+if [ -z "$2" ]
+then
+    TYPE="patch"
+else
+    TYPE=$2
 fi
 
 cd ..
@@ -26,16 +33,16 @@ README=$PROJECT_PATH/$1/README.md
 
 echo "============================================"
 echo "repo: $1"
+echo $TYPE
 echo "============================================"
 
 cd $DIR
 git pull
 
-docker run --rm -v $DIR:/app treeder/bump patch
-version=`cat VERSION`
+docker run --rm -v $DIR:/app treeder/bump $TYPE
+version=`cat $DIR/VERSION`
 
-echo "update version number: $version"
-
+sh build.sh
 sh jsdoc.sh
 
 git add -A
@@ -49,8 +56,7 @@ docker push $USERNAME/$1:latest
 
 echo "============================================"
 echo "repo: $1"
+echo "release type: $TYPE"
 echo "version: $version"
-echo "finished release"
+echo "finished"
 echo "============================================"
-
-cd release
